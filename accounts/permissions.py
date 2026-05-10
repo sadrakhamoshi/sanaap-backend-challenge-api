8088a63710159eb1406a59578920f6af1d361e93
sadrakhamoshi <sadrakhamoshi7@gmail.com>
2026-05-10 14:20:32 -0700

    feat (user): initial add permissions for document changes for differen roles admin/editor/viewer

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,28 +1,5 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated and request.user.is_admin)
-
-
-class IsEditorRole(BasePermission):
-    def has_permission(self, request, view):
-        if not (request.user and request.user.is_authenticated):
-            return False
-        
-        # Editors cannot delete
-        if request.method == 'DELETE':
-            return False
-            
-        return bool(request.user.is_admin or request.user.is_editor)
-
-class IsViewerRole(BasePermission):
-    def has_permission(self, request, view):
-        if not (request.user and request.user.is_authenticated):
-            return False
-            
-        # Viewers can only use safe methods (GET, HEAD, OPTIONS)
-        if request.method in SAFE_METHODS:
-            return True
-            
-        return bool(request.user.is_admin or request.user.is_editor)
--- documents/views.py
+++ documents/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from documents.models import Document
 from documents.serializers import DocumentSerializer
+from rest_framework.permissions import IsAuthenticated
+from documents.permissions import DocumentRolePermission
+
 
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
+
+    permission_classes = [IsAuthenticated, DocumentRolePermission]
