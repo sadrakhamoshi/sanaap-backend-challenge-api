151a39eb5f0956d0146521fe84c6d70400b94718
sadrakhamoshi <sadrakhamoshi7@gmail.com>
2026-05-10 16:41:05 -0700

    feat (document): signed the documents urls with ttl 120 sec

--- documents/serializers.py
+++ documents/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import Document
 
 class DocumentSerializer(serializers.ModelSerializer):
+    download_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Document
-        fields = ['id', 'content', 'original_name', 'size', 'content_type']
+        fields = ['id', 'download_url', 'original_name', 'size', 'content_type']
         read_only_fields = ['original_name', 'size', 'content_type']
+
+    def get_download_url(self, obj):
+        if obj.file and hasattr(obj.file, 'url'):
+            return obj.file.url
+        return None
