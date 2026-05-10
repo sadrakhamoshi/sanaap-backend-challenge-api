226e9e159a2e35f8032b986a8e5dcf48a7c8f4c0
sadrakhamoshi <sadrakhamoshi7@gmail.com>
2026-05-10 16:53:50 -0700

    feat (api): add swagger doc for the apis

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('documents/', include('documents.urls')),
     path('auth/', include('accounts.urls')),
+
+    # swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    # 2. The Swagger UI interface
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
