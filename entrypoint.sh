565447935b4e3618535fc3849439044d9dce88ce
sadrakhamoshi <sadrakhamoshi7@gmail.com>
2026-05-12 14:34:59 -0700

    fix(docker) : finilize the docker and docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,5 @@ echo "📁 Collecting static files..."
 python manage.py collectstatic --noinput
 
 
-# Launch Django with Gunicorn
-echo "🌟 Starting Django with Gunicorn..."
-exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 120
+echo "✨ Done setting up djagno project..."
+exec "$@"
