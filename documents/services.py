5cee4ee8b1b6d5a37e362075e3f2e8d1dca8d7ab
sadrakhamoshi <sadrakhamoshi7@gmail.com>
2026-05-12 04:31:53 -0700

    feat(notification): add websocket service for AudiLogerService to send the notifications

--- documents/services.py
+++ documents/services.py
@@ -1,6 +1,8 @@
 from typing import Optional, Dict, Any
 from django.contrib.auth import get_user_model
-from documents.models import AuditLog, Document
+from documents.models import AuditAction, AuditLog, Document
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 User = get_user_model()
 
@@ -16,3 +18,18 @@ class AuditLoggerService:
             action=action,
             metadata=metadata
         )
+        if action == AuditAction.CREATED.value:
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                "admin_notifications",
+                {
+                    "type": "send_notification", 
+                    "payload": {
+                        "event": "DOCUMENT_UPLOADED",
+                        "document_id": document.id,
+                        "document_name": document.original_name,
+                        "uploaded_by": user.username if user else "System",
+                        "size": document.size
+                    }
+                }
+            )
